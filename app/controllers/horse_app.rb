98326299279e0140c923770b1d79dee04b81ccbb
Ashish Vaid <vaidashi@Ashishs-MacBook-Air.local>
2017-06-28 21:48:01 -0600

    winning amount and average working

--- app/controllers/horse_app.rb
+++ app/controllers/horse_app.rb
@@ -5,8 +5,11 @@ class HorseApp < Sinatra::Base
     erb :"horses/index"
   end
 
-  get 'jockey/:id' do |id|
+  get '/jockey/:id' do |id|
     @jockey = Jockey.find(id)
+    @horses = @jockey.horses
+    @jockey_winnings = @horses.sum("total_winnings")
+    @jockey_winning_average = @horses.average("total_winnings")
     erb :"jockeys/show"
   end
 
